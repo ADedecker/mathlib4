9655a6ccfa44d579f9b94da5a894294af099fe62
ADedecker <anatolededecker@gmail.com>
2026-04-04 21:03:06 +0200

    I'm stupid

--- Mathlib/Topology/Algebra/Module/Spaces/Flip.lean
+++ Mathlib/Topology/Algebra/Module/Spaces/Flip.lean
@@ -51,14 +51,34 @@ def flipPointwiseUniform {𝔖 : Set (Set N)} (h𝔖 : ⋃₀ 𝔖 = univ) :
   map_add' _ _ := by ext; rfl
   map_smul' _ _ := by ext; rfl
   cont := by
-    set Φ : (M →SLₚₜ[ρ₁₂] P) →L[R₂] (M → P) := .pi (PointwiseConvergenceCLM.evalCLM ρ₁₂ _)
-    have : IsEmbedding Φ := PointwiseConvergenceCLM.isEmbedding_coeFn ρ₁₂ M P
-    rw [UniformConvergenceCLM.isEmbedding_postcomp σ₁₂ Φ this _ |>.continuous_iff]
-    have := UniformConvergenceCLM.isEmbedding_postcomp ρ₁₂ Φ this (_ : Set (Set M))
-    simp only [PointwiseConvergenceCLM.isEmbedding_coeFn _ _ _ |>.continuous_iff, continuous_pi_iff]
-    intro n m
-    change Continuous fun (L : M →SLₚₜ[ρ₁₂] N →SLₚₜ[σ₁₂] P) ↦ L m n
-    fun_prop
+    let _ := IsTopologicalAddGroup.rightUniformSpace P
+    have _ : IsUniformAddGroup P := isUniformAddGroup_of_addCommGroup
+    have : IsUniformEmbedding ((↑) : (M →SLₚₜ[ρ₁₂] P) → M → P) :=
+      PointwiseConvergenceCLM.isUniformEmbedding_coeFn ρ₁₂ M P
+    rw [UniformConvergenceCLM.isEmbedding_coeFn _ _ _ |>.continuous_iff,
+      UniformOnFun.postcomp_isUniformEmbedding this |>.isEmbedding.continuous_iff,
+      UniformOnFun.uniformEquivPiComm _ _ |>.isUniformEmbedding.isEmbedding.continuous_iff,
+      continuous_pi_iff]
+    exact fun m ↦ UniformConvergenceCLM.isEmbedding_coeFn _ _ _ |>.continuous.comp
+      <| continuous_eval_const m
+
+def flipUniformPointwise {𝔖 : Set (Set M)} (h𝔖 : ⋃₀ 𝔖 = univ) :
+    (M →SLᵤ[ρ₁₂, 𝔖] N →SLₚₜ[σ₁₂] P) →L[A] (N →SLₚₜ[σ₁₂] M →SLᵤ[ρ₁₂, 𝔖] P) where
+  toFun L :=
+    letI Lₗ : M →ₛₗ[ρ₁₂] N →ₛₗ[σ₁₂] P := ContinuousLinearMap.coeLMₛₗ _ ∘ₛₗ L.toLinearMap
+    { toFun n :=
+      { toFun m := L m n
+        map_add' _ _ := Lₗ.map_add₂ ..
+        map_smul' _ _ := Lₗ.map_smulₛₗ₂ ..
+        cont := by fun_prop }
+      map_add' _ _ := by ext; exact (Lₗ _).map_add ..
+      map_smul' _ _ := by ext; exact (Lₗ _).map_smulₛₗ ..
+      cont := sorry }
+  map_add' _ _ := by ext; rfl
+  map_smul' _ _ := by ext; rfl
+  cont := PointwiseConvergenceCLM.continuous_of_continuous_eval fun n ↦ by
+
+    sorry
 
 def flipPointwisePointwise : (M →SLₚₜ[ρ₁₂] N →SLₚₜ[σ₁₂] P) →L[A] (N →SLₚₜ[σ₁₂] M →SLₚₜ[ρ₁₂] P) where
   toFun L :=
